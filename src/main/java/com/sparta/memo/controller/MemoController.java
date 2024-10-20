3f215c1abbca7035108475e58c9ed6433ece5130
JISU-YANG <jisu@ego.so>
2024-10-20 09:54:03 +0900

    역할 분리하기

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -2,66 +2,42 @@ package com.sparta.memo.controller;
 
 import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
-import com.sparta.memo.entity.Memo;
+import com.sparta.memo.service.MemoService;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-//@RestController
-//@RequestMapping("/api")
-//public class MemoController {
-//
-//    private final Map<Long, Memo> memoList = new HashMap<>();
-//
-//    @PostMapping("/memos")
-//    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
-//        // RequestDto -> Entity
-//        Memo memo = new Memo(requestDto);
-//
-//        // Memo Max ID Check
-//        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
-//        memo.setId(maxId);
-//
-//        // DB 저장
-//        memoList.put(memo.getId(), memo);
-//
-//        // Entity -> ResponseDto
-//        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
-//
-//        return memoResponseDto;
-//    }
-//
-//    @GetMapping("/memos")
-//    public List<MemoResponseDto> getMemos() {
-//        // Map to List
-//        List<MemoResponseDto> responseList = memoList.values().stream()
-//                .map(MemoResponseDto::new).toList();
-//        return responseList;
-//    }
-//
-//    @PutMapping("/memos/{id}")
-//    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
-//        // 해당 메모가 DB에 존재하는지 확인
-//        if (memoList.containsKey(id)) {
-//            Memo memo = memoList.get(id);
-//
-//            memo.update(requestDto);
-//            return id;
-//        } else {
-//            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-//        }
-//    }
-//
-//    @DeleteMapping("/memos/{id}")
-//    public Long deleteMemo(@PathVariable Long id) {
-//        if (memoList.containsKey(id)) {
-//            memoList.remove(id);
-//            return id;
-//        } else {
-//            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-//        }
-//    }
-//}
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemoController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @PostMapping("/memos")
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+        return new MemoService(jdbcTemplate).creatMemo(requestDto);
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos() {
+        return new MemoService(jdbcTemplate).getMemo();
+    }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        return new MemoService(jdbcTemplate).updateMemo(id, requestDto);
+
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        return new MemoService(jdbcTemplate).deleteMemo(id);
+    }
+
+
+}
